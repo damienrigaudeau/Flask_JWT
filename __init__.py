d15331a97f41eb2b012a408287c6e17b85def511
damienrigaudeau <91134839+damienrigaudeau@users.noreply.github.com>
2025-03-11 14:54:11 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,26 +1,20 @@
 from flask import Flask
-from flask import render_template
-from flask import json
-from flask import jsonify
-from flask import request
+from flask import render_template, jsonify, request
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, JWTManager
+from datetime import timedelta
 
-from flask_jwt_extended import create_access_token
-from flask_jwt_extended import get_jwt_identity
-from flask_jwt_extended import jwt_required
-from flask_jwt_extended import JWTManager
-                                                                                                                                       
-app = Flask(__name__)                                                                                                                  
-                                                                                                                                       
-# Configuration du module JWT!!
-app.config["JWT_SECRET_KEY"] = "Ma_clé_secrete"  # Ma clée privée
+app = Flask(__name__)
+
+# Configuration du module JWT
+app.config["JWT_SECRET_KEY"] = "Ma_clé_secrete"  # Clé secrète
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)  # Expiration du jeton après 1 heure
 jwt = JWTManager(app)
 
 @app.route('/')
 def hello_world():
     return render_template('accueil.html')
 
-# Création d'une route qui vérifie l'utilisateur et retour un Jeton JWT si ok.
-# La fonction create_access_token() est utilisée pour générer un jeton JWT.
+# Route pour vérifier l'utilisateur et retourner un Jeton JWT si ok.
 @app.route("/login", methods=["POST"])
 def login():
     username = request.json.get("username", None)
@@ -31,13 +25,12 @@ def login():
     access_token = create_access_token(identity=username)
     return jsonify(access_token=access_token)
 
-
 # Route protégée par un jeton valide
 @app.route("/protected", methods=["GET"])
 @jwt_required()
 def protected():
     current_user = get_jwt_identity()
     return jsonify(logged_in_as=current_user), 200
-                                                                                                               
+
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
